54262e82e32db08d5fc0f46cbd0dc3993d8b545f
owenDevEirePersonal <owen@deveire.com>
2017-04-28 14:52:01 +0100

    <Removed Location Updates> Removed Redundent code that called periodic location updates from the googleAPI. The code was unnessary as the manager doesn't need to periodically be updated and will instead the managers location is gotten at startup and left at that.
    #Removed Redundent Code: AddressResultReceiver class
    #Removed Redundent Code: LocationRequest request
    #Removed Redundent Code: ResultCallback<LocationSettingsResult>
    #Removed Redundent Code: LocationSerices.FusedLocationApi.requestLocationUpdates
    #Removed Redundent Code: onLocationChanged(Location location)
    #Removed Redundent Code: startIntentService()

--- app/src/main/java/com/deveire/dev/truckytrack/ManagerActivity.java
+++ app/src/main/java/com/deveire/dev/truckytrack/ManagerActivity.java
@@ -2,20 +2,15 @@ package com.deveire.dev.truckytrack;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentSender;
 import android.content.pm.PackageManager;
-import android.location.Geocoder;
 import android.location.Location;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Handler;
-import android.os.ResultReceiver;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -26,31 +21,22 @@ import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResult;
-import com.google.android.gms.location.LocationSettingsStates;
-import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.gson.internal.bind.ReflectiveTypeAdapterFactory;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-public class ManagerActivity extends FragmentActivity implements AdapterView.OnItemSelectedListener, OnMapReadyCallback, GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks, LocationListener, DownloadCallback<String>
+public class ManagerActivity extends FragmentActivity implements AdapterView.OnItemSelectedListener, OnMapReadyCallback, GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks, DownloadCallback<String>
 {
 
     private GoogleMap mMap;
@@ -68,12 +54,9 @@ public class ManagerActivity extends FragmentActivity implements AdapterView.OnI
 
     //[Network and periodic location update, Variables]
     private GoogleApiClient mGoogleApiClient;
-    private Location locationReceivedFromLocationUpdates;
-    private ManagerActivity.AddressResultReceiver geoCoderServiceResultReciever;
+    private Location usersLocation;
     private int locationScanInterval;
-
-    LocationRequest request;
-    private final int SETTINGS_REQUEST_ID = 8888;
+    
     private final String SAVED_LOCATION_KEY = "79";
 
     private int pingingServerFor;
@@ -145,53 +128,8 @@ public class ManagerActivity extends FragmentActivity implements AdapterView.OnI
         locationScanInterval = 60;//in seconds
 
 
-        request = new LocationRequest();
-        request.setInterval(locationScanInterval * 1000);//in mileseconds
-        request.setFastestInterval(5000);//caps how fast the locations are recieved, as other apps could be triggering updates faster than our app.
-        request.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY); //accurate to 100 meters.
 
-        LocationSettingsRequest.Builder requestBuilder = new LocationSettingsRequest.Builder().addLocationRequest(request);
 
-        PendingResult<LocationSettingsResult> result =
-                LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient,
-                        requestBuilder.build());
-
-        result.setResultCallback(new ResultCallback<LocationSettingsResult>()
-        {
-            @Override
-            public void onResult(@NonNull LocationSettingsResult aResult)
-            {
-                final Status status = aResult.getStatus();
-                final LocationSettingsStates states = aResult.getLocationSettingsStates();
-                switch (status.getStatusCode())
-                {
-                    case LocationSettingsStatusCodes.SUCCESS:
-                        // All location settings are satisfied. The client can
-                        // initialize location requests here.
-
-                        break;
-                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
-                        // Location settings are not satisfied, but this can be fixed
-                        // by showing the user a dialog.
-                        try
-                        {
-                            // Show the dialog by calling startResolutionForResult(),
-                            // and check the result in onActivityResult().
-                            status.startResolutionForResult(ManagerActivity.this, SETTINGS_REQUEST_ID);
-                        } catch (IntentSender.SendIntentException e)
-                        {
-                            // Ignore the error.
-                        }
-                        break;
-                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
-                        // Location settings are not satisfied. However, we have no way
-                        // to fix the settings so we won't show the dialog.
-                        break;
-                }
-            }
-        });
-
-        geoCoderServiceResultReciever = new ManagerActivity.AddressResultReceiver(new Handler());
 
         restoreSavedValues(savedInstanceState);
 
@@ -464,63 +402,14 @@ public class ManagerActivity extends FragmentActivity implements AdapterView.OnI
     {
         if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
         {
-            locationReceivedFromLocationUpdates = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, request, this);
-            if(locationReceivedFromLocationUpdates != null)
-            {
-                //YES, lat and long are multi digit.
-                if(Geocoder.isPresent())
-                {
-                    startIntentService();
-                }
-                else
-                {
-                    Log.e("ERROR:", "Geocoder is not avaiable");
-                }
-            }
-            else
-            {
-
-            }
-
-
+            usersLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
         }
-
-
-
     }
 
     @Override
     public void onConnectionSuspended(int i)
     {
         //put other stuff here
-    }
-
-    //update app based on the new location data, and then begin pinging servlet with the new location
-    @Override
-    public void onLocationChanged(Location location)
-    {
-        locationReceivedFromLocationUpdates = location;
-        userLocation = location;
-        //locationReceivedFromLocationUpdates = fakeUserLocation;
-
-
-        if(locationReceivedFromLocationUpdates != null)
-        {
-
-
-            //startDownload();
-        }
-        else
-        {
-            /*serverURL = "http://geo.dev.deveire.com/store/location?id=" + Settings.Secure.ANDROID_ID.toString() + "&lat=" + 52.67 + "&lon=" + -8.54;
-            //lat and long are doubles, will cause issue? nope
-            Log.i("Network Update", "Attempting to start download from onLocationChanged." + serverURL);
-            aNetworkFragment = NetworkFragment.getInstance(getSupportFragmentManager(), serverURL);*/
-            //startDownload();
-            Log.e("ERROR", "Unable to send location to sevrver, current location = null");
-        }
-
     }
 
     @Override
@@ -533,7 +422,7 @@ public class ManagerActivity extends FragmentActivity implements AdapterView.OnI
     @Override
     public void onSaveInstanceState(Bundle savedState)
     {
-        savedState.putParcelable(SAVED_LOCATION_KEY, locationReceivedFromLocationUpdates);
+        savedState.putParcelable(SAVED_LOCATION_KEY, usersLocation);
         super.onSaveInstanceState(savedState);
     }
 
@@ -548,17 +437,10 @@ public class ManagerActivity extends FragmentActivity implements AdapterView.OnI
             {
                 // Since LOCATION_KEY was found in the Bundle, we can be sure that
                 // mCurrentLocationis not null.
-                locationReceivedFromLocationUpdates = savedInstanceState.getParcelable(SAVED_LOCATION_KEY);
+                usersLocation = savedInstanceState.getParcelable(SAVED_LOCATION_KEY);
             }
 
         }
-    }
-
-    protected void startIntentService() {
-        Intent intent = new Intent(this, geoCoderIntent.class);
-        intent.putExtra(Constants.RECEIVER, geoCoderServiceResultReciever);
-        intent.putExtra(Constants.LOCATION_DATA_EXTRA, locationReceivedFromLocationUpdates);
-        startService(intent);
     }
 
     //Update activity based on the results sent back by the servlet.
@@ -676,32 +558,7 @@ public class ManagerActivity extends FragmentActivity implements AdapterView.OnI
         }
     }
 
-    class AddressResultReceiver extends ResultReceiver
-    {
-        public AddressResultReceiver(Handler handler) {
-            super(handler);
-        }
 
-        @Override
-        protected void onReceiveResult(int resultCode, Bundle resultData) {
-
-            // Display the address string
-            // or an error message sent from the intent service.
-            resultData.getString(Constants.RESULT_DATA_KEY);
-
-
-            // Show a toast message if an address was found.
-            if (resultCode == Constants.SUCCESS_RESULT)
-            {
-                Log.i("Success", "Address found");
-            }
-            else
-            {
-                Log.e("Network Error:", "in OnReceiveResult in AddressResultReceiver: " +  resultData.getString(Constants.RESULT_DATA_KEY));
-            }
-
-        }
-    }
 //**********[/Location Update and server pinging Code]
 
     class mapScrolledListener implements GoogleMap.OnCameraIdleListener
